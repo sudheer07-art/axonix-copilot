f1a017b7cc547f9fa52e0446bfd4449a05765aab
sudheer gits <sudheercheerla07@icloud.com>
2026-07-05 09:25:13 +0530

    Debug Adzuna

--- app/main.py
+++ app/main.py
@@ -1197,7 +1197,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5500",
-        "http://127.0.0.1:5500"
+        "http://127.0.0.1:5500",
+        "https://axonix-copilot.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
